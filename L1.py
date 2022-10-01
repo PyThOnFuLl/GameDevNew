336ffa93959631874771c1bf884d4ae4f4638afb
PyThOnFuLl <100462831+PyThOnFuLl@users.noreply.github.com>
2022-10-02 01:04:26 +0300

    (not)understanding...

---
+++ L1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [3,21,22,34,54,34,55,67,89,99]
+x = np.array(x)
+y = [2,22,24,65,79,82,55,130,150,199]
+y = np.array(y)
+
+plt.scatter(x,y)
